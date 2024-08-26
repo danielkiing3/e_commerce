664049e40dc6f586faeeba252fa010a8ede3569a
Daniel Olayinka <124001312+danielkiing3@users.noreply.github.com>
2024-08-26 15:38:50 +0100

    Add Google Sign In fun

--- lib/data/repositories/authenication/authenication_repository.dart
+++ lib/data/repositories/authenication/authenication_repository.dart
@@ -7,10 +7,12 @@ import 'package:e_commerce/utils/expections/firebase_exceptions.dart';
 import 'package:e_commerce/utils/expections/format_exceptions.dart';
 import 'package:e_commerce/utils/expections/platform_exceptions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthenicationRepository extends GetxController {
   static AuthenicationRepository get instance => Get.find();
@@ -113,11 +115,41 @@ class AuthenicationRepository extends GetxController {
 
   /* ---------------- Password identity & Social sign in ----------------- */
   /// [GoogleAuthenication] - GOOGLE
+  Future<UserCredential> signInWithGoogle() async {
+    try {
+      // Trigger the authenication follow
+      final GoogleSignInAccount? userAccount = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await userAccount?.authentication;
+
+      // Create a new credential
+      final AuthCredential credentials = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
+
+      // Once signed in, return the UserCredentails
+      return await _auth.signInWithCredential(credentials);
+    } on FirebaseAuthException catch (e) {
+      throw UFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw UFirebaseException(e.code).message;
+    } on FormatException {
+      throw const UFormatException();
+    } on PlatformException catch (e) {
+      throw UPlatformException(e.code).message;
+    } catch (e) {
+      if (kDebugMode) print('Something went wrong: $e');
+      throw 'Something went wrong. Please try again';
+    }
+  }
 
   /// [FacebookAuthenication] - FACEBOOK
+  ///
+  /// [AppleAuthenication] - FACEBOOK
 
   /* -------------------- Email & Password Sign in --------------------- */
-  /// [Logout Users] - Valid for any authenication
+  /// [LogoutUsers] - Valid for any authenication
   Future<void> logout() async {
     try {
       await _auth.signOut();
@@ -135,5 +167,5 @@ class AuthenicationRepository extends GetxController {
     }
   }
 
-  /// [Delete User ] - Remove user auth and Firebase Account
+  /// [DeleteUser] - Remove user auth and Firebase Account
 }
